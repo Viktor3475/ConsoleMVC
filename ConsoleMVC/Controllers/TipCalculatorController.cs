329a2f202c4e8857e6bf67def5f386f88c9df798
Viktor3475 <viktordachev17b@gmail.com>
2023-06-19 23:52:52 +0300

    Add project files.

---
+++ ConsoleMVC/Controllers/TipCalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleMVC.Model;
+using ConsoleMVC.Views;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleMVC.Controllers
+{
+    class TipCalculatorController
+    {
+        private Tip tip;
+        private Display display;
+
+        public TipCalculatorController()
+        {
+            display = new Display();
+            tip = new Tip(display.Amount, display.Percent);
+            display.TipAmount = tip.CalculateTip();
+            display.Total = tip.CalculateTotal();
+            display.ShowTipAndTotal();
+        }
+    }
+}
